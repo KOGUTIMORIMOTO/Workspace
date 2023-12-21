e7340d1e42e5439eb2a515e6cba2c5528675f974
KOGUTIMORIMOTO <ayataro0903@gmail.com>
2023-12-21 15:34:37 +0900

    ホームへボタン追加

--- hello/hello2/imdex.js
+++ hello/hello2/imdex.js
@@ -1,31 +1,23 @@
 function saveData1() {
-    // 入力されたデータを取得
     var inputData1 = document.getElementById('dataInput1').value;
 
-    // ローカルストレージにデータを保存
     localStorage.setItem('savedData1', inputData1);
 }
 
 function saveData2() {
-    // 入力されたデータを取得
     var inputData2 = document.getElementById('dataInput2').value;
 
-    // ローカルストレージにデータを保存
     localStorage.setItem('savedData2', inputData2);
 }
 
 function saveData3() {
-    // 入力されたデータを取得
     var inputData3 = document.getElementById('dataInput3').value;
 
-    // ローカルストレージにデータを保存
     localStorage.setItem('savedData3', inputData3);
 }
 
 function saveData4() {
-    // 入力されたデータを取得
     var inputData4 = document.getElementById('dataInput4').value;
 
-    // ローカルストレージにデータを保存
     localStorage.setItem('savedData4', inputData4);
 }
