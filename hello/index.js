59f3f7e9df6463862d8e596c9631dae5167f3e16
KOGUTIMORIMOTO <ayataro0903@gmail.com>
2023-12-21 22:49:18 +0900

    色など

--- hello/index.js
+++ hello/index.js
@@ -28,3 +28,19 @@ document.addEventListener('DOMContentLoaded', function() {
     var savedData1_1 = localStorage.getItem('savedData1_1');
     document.getElementById('dataInput1_1').value = savedData1_1 || 'タイトルなし'; 
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var savedData2_2 = localStorage.getItem('savedData2_2');
+    document.getElementById('dataInput2_2').value = savedData2_2 || 'タイトルなし'; 
+ });
+        
+document.addEventListener('DOMContentLoaded', function() {
+    var savedData3_3 = localStorage.getItem('savedData3_3');
+    document.getElementById('dataInput3_3').value = savedData3_3 || 'タイトルなし'; 
+});
+        
+document.addEventListener('DOMContentLoaded', function() {
+    var savedData4_4 = localStorage.getItem('savedData4_4');
+    document.getElementById('dataInput4_4').value = savedData4_4 || 'タイトルなし'; 
+});
+        
